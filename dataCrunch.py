0d2a783ab0fae6f1e2e158807d6911bd8229248d
Jordan <jordan3steele@gmail.com>
2017-01-28 14:56:22 -0800

    Updated join table function, command line arguments
    dataCrunch.py now takes a command line argument of the cvs file to be
    parsed.
    
    joins table detailed_data with group_data based on client id

--- dataCrunch.py
+++ dataCrunch.py
@@ -14,8 +14,7 @@ def csvWriter(path, outputData):
 
 #strips key from first part of data file
 def splitTables(data):
-	data = filter(None, data)
-
+	
 	i = 0
 	for row in data:
 		if row[0] == "GROUP_DATA":
@@ -36,57 +35,67 @@ def splitTables(data):
 	detailTable = [data[y] for y in xrange(detailStart, totalStart)]
 	totalTable = [data[z] for z in xrange(totalStart, len(data))]
 
-
 	return keyTable, detailTable, totalTable
-
-
-
-
-
-
 
 def main(): 
 
+	#open data file
 
-	#open data file 
-	with open('detailsOfContributionsFromAssociations.csv', 'rb') as csvFile:
-	    	fileReader = csv.reader(csvFile)
-	    	data = [row for row in csv.reader(csvFile.read().splitlines())]
+	fileName = sys.argv[1]
+	with open(fileName, 'rb') as csvFile:
+		fileReader = csv.reader(csvFile)
+		data = [row for row in csv.reader(csvFile.read().splitlines())]
 
 
-	"""
-	#fix the date on the contributions table
-	for row in data:
-		parsedYear = row[3][-2:]
-		if parsedYear == "":
-			year = ""
-		else:
-			year = '20' + parsedYear
-		row.append(year)
-
-	#append the tables based on Client ID
-	for dataRow in data:
-		for keyRow in key:
-			if keyRow[0] == dataRow[0]:
-				i = 0
-				for item in keyRow:
-				 dataRow.insert(i,keyRow[i])
-				 i = i+1
-	"""
-
+	data = filter(None, data)
 	tables = splitTables(data)
 	
-	for x in tables[1]:
-		print x
-
+#sort key table
+	groupData = tables[0]	
 	
+	#header = groupData[:1]
+	groupData = groupData[2:]
+
+	groupData.sort(key = lambda row: row[0])	
+	#groupData = header + groupData
+
+#sort detailTable
+	detailTable = tables[1]
+	
+	#header2 = detailTable[:1]
+	detailTable = detailTable[2:]
+
+	detailTable.sort(key = lambda row: row[0])
+	#detailTable = hleader2 + detailTable
+
+#match two lists
+	
+	finalCopy = []
+	#append the tables based on Client ID
+	i = 0
+	for dataRow in detailTable:
+		for keyRow in groupData:
+			if keyRow[0] == dataRow[0]:
+				joinedL = keyRow + dataRow
+				print joinedL
+				finalCopy.append(joinedL)
+
+
+		i = i + 1
+
+
+
+
+
 
 	outputFile = 'output.csv'
-	csvWriter(outputFile, data)
+	csvWriter(outputFile, finalCopy)
 
 
 if __name__ == "__main__":
 	main()
+
+
 
 
 
